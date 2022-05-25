cf39eadb2a2f092dd359786ba14f352c46d5d932
Bety Martinez <trism460@gmail.com>
2022-05-24 20:58:24 -0500

    Generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { ProgramsComponent } from './components/programs/programs.component';
+import { ExperiencesComponent } from './components/experiences/experiences.component';
+import { ForumComponent } from './components/forum/forum.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { UserComponent } from './components/user/user.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    ProgramsComponent,
+    ExperiencesComponent,
+    ForumComponent,
+    FooterComponent,
+    UserComponent,
+    StatisticsComponent
   ],
   imports: [
     BrowserModule,
