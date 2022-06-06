c0e1bb5fb9961629d3d30a66c356f6fa80235d17
Bety Martinez <trism460@gmail.com>
2022-06-05 19:16:47 -0500

    Name for inputs

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,8 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginEmail: string;
+  loginPassword: string;
+  registerEmail: string;
+  registerPassword: string;
+  registerPasswordConf: string;
+  registerName: string;
+  registerLastName: string;
+  registerBirthday: Date;
+  registerPhone: string;
+  registerCareer: string;
+  constructor() {
+    this.loginEmail = '';
+    this.loginPassword = '';
+    this.registerEmail = '';
+    this.registerPassword = '';
+    this.registerPasswordConf = '';
+    this.registerName = '';
+    this.registerLastName = '';
+    this.registerBirthday = new Date();
+    this.registerPhone = '';
+    this.registerCareer = '';
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
