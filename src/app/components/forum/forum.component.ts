e6b60e053c5298dde64f5b0c29b7b3d4d0b5fc94
Bety Martinez <trism460@gmail.com>
2022-06-20 17:55:53 -0500

    Publish a question

--- src/app/components/forum/forum.component.ts
+++ src/app/components/forum/forum.component.ts
@@ -9,15 +9,37 @@ import { ApiForumService } from 'src/app/services/api-forum.service';
 export class ForumComponent implements OnInit {
   questions: Array<any> = [];
   showFormAdd: boolean;
+  newQuestion: string;
+  newStatus: string;
+  newTopic: string;
 
   constructor(private apiForumService: ApiForumService) {
     this.showFormAdd = false;
+    this.newQuestion = '';
+    this.newStatus = '';
+    this.newTopic = '';
     setTimeout(() => {
       this.questions = apiForumService.questions;
     }, 1000);
   }
+
   addQuestion() {
-    console.log("question")
+    if (this.newQuestion !== '' && this.newStatus !== '' && this.newTopic !== '') {
+      const NEW_QUESTION = {
+        strStatus: this.newStatus,
+        arrAnswers: [],
+        strTopic: this.newTopic,
+        strQuestion:  this.newQuestion,
+        dateDate: new Date(),
+        idUser: localStorage.getItem("userUTraveler")
+      };
+      this.apiForumService.addQuestion(NEW_QUESTION);
+      this.apiForumService.getData();
+      alert('Your question has been published!');
+      window.location.reload();
+    } else {
+      alert('Complete the fields correctly');
+    }
   }
   showForm() {
     this.showFormAdd = true;
