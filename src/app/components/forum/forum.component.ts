babc88b1d4a71ff312d0565a2ae29b426f3d2c90
Bety Martinez <trism460@gmail.com>
2022-06-20 19:52:37 -0500

    Filter questions

--- src/app/components/forum/forum.component.ts
+++ src/app/components/forum/forum.component.ts
@@ -7,19 +7,23 @@ import { ApiForumService } from 'src/app/services/api-forum.service';
   styleUrls: ['./forum.component.css']
 })
 export class ForumComponent implements OnInit {
+  ALL_QUESTIONS: Array<any> = [];
   questions: Array<any> = [];
   showFormAdd: boolean;
   newQuestion: string;
   newStatus: string;
   newTopic: string;
+  filterBy: any;
 
   constructor(private apiForumService: ApiForumService) {
     this.showFormAdd = false;
     this.newQuestion = '';
     this.newStatus = '';
     this.newTopic = '';
+    this.filterBy = '';
     setTimeout(() => {
       this.questions = apiForumService.questions;
+      this.ALL_QUESTIONS = this.questions;
     }, 1000);
   }
 
@@ -43,6 +47,31 @@ export class ForumComponent implements OnInit {
   }
   showForm() {
     this.showFormAdd = true;
+  }
+  filterQuestions() {
+    this.filterBy = (<HTMLInputElement>document.getElementById("selectFilter")).value;
+    this.questions = this.ALL_QUESTIONS.filter((q) => {
+      switch(this.filterBy) {
+        case "1.0":
+          return this.questions;
+        case "1.1":
+          return q.strStatus === 'Solved';
+        case "1.2":
+          return q.strStatus === 'Unsolved';
+        case "1.3":
+          return q.strStatus === 'Urgent';
+        case "1.4":
+          return q.strStatus === 'Without answers';
+        case "2.1":
+          return q.strTopic === 'Costs';
+        case "2.2":
+          return q.strTopic === 'Dates';
+        case "2.3":
+          return q.strTopic === 'Others';
+        default:
+          return this.questions;
+      }
+    });
   }
   ngOnInit(): void {
   }
