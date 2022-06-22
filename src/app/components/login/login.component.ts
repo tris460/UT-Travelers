d7b75e08809a39cd88fbc0fcecf97ec189d4244a
Bety Martinez <trism460@gmail.com>
2022-06-22 18:33:34 -0500

    Default profile picture link

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -74,7 +74,8 @@ export class LoginComponent implements OnInit {
       strLastName: this.registerLastName,
       dateBirth: this.registerBirthday,
       strPhone: this.registerPhone,
-      strCareer: this.registerCareer
+      strCareer: this.registerCareer,
+      strPhoto: "https://media.istockphoto.com/vectors/no-image-available-sign-vector-id922962354?k=20&m=922962354&s=612x612&w=0&h=f-9tPXlFXtz9vg_-WonCXKCdBuPUevOBkp3DQ-i0xqo="
     }
     console.log(NEW_USER)
   }
