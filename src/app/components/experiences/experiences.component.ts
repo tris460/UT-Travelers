2aaca54f1d217fdd21bfc88c56bc11cdba2600f4
Bety Martinez <trism460@gmail.com>
2022-06-22 22:19:36 -0500

    Next button of carousel

--- src/app/components/experiences/experiences.component.ts
+++ src/app/components/experiences/experiences.component.ts
@@ -8,10 +8,22 @@ import { ApiExperiencesService } from 'src/app/services/api-experiences.service'
 })
 export class ExperiencesComponent implements OnInit {
   experiencesList: Array<any> = [];
+  experienceStart: number = 0;
+  experienceEnd: number = 0;
+  experiencesLength: number = 0;
+  experiencesListShown: Array<any> = [];
 
   constructor(private apiExperiencesService: ApiExperiencesService) {
     setTimeout(() => {
       this.experiencesList = apiExperiencesService.experiences;
+      this.experiencesLength = this.experiencesList.length;
+      if(this.experiencesLength >= 3) {
+        this.experienceEnd = 3;
+        this.experiencesListShown = this.experiencesList.slice(0,this.experienceEnd);
+      } else {
+        this.experienceEnd = this.experiencesLength;
+        this.experiencesListShown = this.experiencesList;
+      }
     }, 1000);
   }
   getContactInformation(index: number) {
@@ -21,6 +33,15 @@ export class ExperiencesComponent implements OnInit {
       const USER_SELECTED = this.experiencesList[index];
       alert(`Email: ${USER_SELECTED.strEmail}\nPhone: ${USER_SELECTED.strPhone}`);
     }
+  }
+  showNext() {
+    if(this.experienceEnd === this.experiencesLength) this.experienceEnd = 0;
+    this.experienceStart += 3;
+    this.experienceEnd += 3;
+    if(this.experienceStart > this.experiencesLength) this.experienceStart = 0;
+    if(this.experienceEnd > this.experiencesLength) this.experienceEnd = this.experiencesLength;
+    this.experiencesListShown = [];
+    this.experiencesListShown = this.experiencesList.slice(this.experienceStart, this.experienceEnd);
   }
   ngOnInit(): void {
   }
