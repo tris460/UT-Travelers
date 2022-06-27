56b41acfb6c06a03a48457b7f97c79ed06ced89c
Bety Martinez <trism460@gmail.com>
2022-06-26 23:01:07 -0500

    Set an account in false

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ApiUserService } from 'src/app/services/api-user.service';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-user',
@@ -17,12 +19,15 @@ export class UserComponent implements OnInit {
   birthday: any;
   phoneNumber: string = '';
   programs: Array<any> = [];
+  password: string = '';
   showInfo: boolean = false;
   addPrevProgram: boolean = false;
   previousProgram: string = '';
+  deleteUser: boolean = false;
+  confirmDelete: string = '';
   userImageView = document.getElementById("Profile-Picture") as HTMLImageElement;
 
-  constructor(private apiUserService: ApiUserService) {
+  constructor(private apiUserService: ApiUserService,private router: Router, private LoginService: LoginService) {
     this.emailActualUser = localStorage.getItem("userUTTraveler")
     this.emailActualUser = JSON.parse(this.emailActualUser).email;
     setTimeout(() => {
@@ -38,9 +43,36 @@ export class UserComponent implements OnInit {
       this.birthday = this.actualUser[0].dateBirth.slice(0,10);
       this.phoneNumber = this.actualUser[0].strPhone;
       this.programs = this.actualUser[0].arrPrograms;
+      this.password = this.actualUser[0].strPassword;
     }, 900);
   }
-  deleteAccount() { }
+  deleteAccount() {
+    if(this.password === this.confirmDelete) {
+      const u = this.actualUser[0];
+      const ID_USER = this.actualUser[0]._id;
+      const NEW_USER = {
+        boolStatus: false,
+        strRol: u.strRol,
+        strName: u.strName,
+        strLastName: u.strLastName,
+        dateBirth: u.dateBirth,
+        strPhone: u.strPhone,
+        strEmail: u.strEmail,
+        strPassword: u.strPassword,
+        strCareer: u.strCareer,
+        strPhoto: u.strPhoto,
+        arrPrograms: u.arrPrograms
+      }
+      this.apiUserService.editUser(ID_USER, NEW_USER);
+      alert("Your account has been deleted successfully.");
+      setTimeout(() => {
+        localStorage.removeItem("userUTTraveler");
+        this.router.navigate(["home"]);
+      }, 500);
+    } else {
+      alert("The password is incorrect, try again.");
+    }
+  }
   addPreviousProgram() {
     if(this.previousProgram == '') {
       alert("Complete the field to continue.");
