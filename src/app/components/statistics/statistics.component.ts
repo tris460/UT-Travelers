40d9d9dd731464f2eccc7ac1de2a1e8503139276
Bety Martinez <trism460@gmail.com>
2022-06-30 13:23:21 -0500

    Show charts in statistics

--- src/app/components/statistics/statistics.component.ts
+++ src/app/components/statistics/statistics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare let google: any;
 
 @Component({
   selector: 'app-statistics',
@@ -7,8 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatisticsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(this.drawChart);
+  }
 
+  drawChart() {
+    let dataCareers = google.visualization.arrayToDataTable([
+      ['Career', 'Students'],
+      ['TIDSM', 11],
+      ['TIARD', 2],
+      ['MAI', 2],
+      ['PA', 2],
+      ['DNAM', 7]
+    ]);
+    let dataPrograms = google.visualization.arrayToDataTable([
+      ['Program', 'Students'],
+      ['Summer Camps in the USA', 11],
+      ['Summer Camps in Canada', 8],
+      ['Work and Travel in the USA', 4],
+      ['Inter and trainee programs', 1],
+    ]);
+    let optionsCareers = {
+      title: 'Careers'
+    };
+    let optionsPrograms = {
+      title: 'Programs'
+    };
+    let chartCareers = new google.visualization.PieChart(<HTMLElement>document.getElementById('piechart-careers'));
+    let chartPrograms = new google.visualization.PieChart(<HTMLElement>document.getElementById('piechart-programs'));
+    chartCareers.draw(dataCareers, optionsCareers);
+    chartPrograms.draw(dataPrograms, optionsPrograms);
+  }
   ngOnInit(): void {
   }
 
